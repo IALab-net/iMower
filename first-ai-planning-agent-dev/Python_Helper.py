dd4c68fa7be39e2e53f491951a395374ceac1fc6
Yoann Benoit <ybenoit@xebia.fr>
2015-06-25 10:59:24 +0200

    Change Helper

--- first-ai-planning-agent-dev/Python_Helper.py
+++ first-ai-planning-agent-dev/Python_Helper.py
@@ -23,27 +23,6 @@ def my_function(arg1, arg2):
 	return my_result
 
 
-# DEFINITION OF A STATE
-
-# ---- Arguments
-grid # The world grid
-_mower_pos # The position of the mower in the grid
-_earth_mode # Boolean, True if the mower can mow until the earth
-_nb_propellers # Number of propellers of the mower
-_bonus_mow # The bonus given when we mow
-_score # The score of the mower
-_penalties # Dictionary of penalties (living_penalty, mow_to_earth, lose_propeller)
-
-# ---- Methods
-def get_grid_size(self): # Height and widht of the grid
-
-def is_game_over(self): # Boolean, True if nb_propellers = 0
-
-def is_win(self): # Boolean, True if there is no mode 'H' in the grid
-
-def execute_action(self, action): # Returns final state given a set of actions
-
-
 # DEFINITION OF A PROBLEM
 
 # ---- Arguments
@@ -70,3 +49,25 @@ def result(self, state, action): # New state tuple, which is the result of the a
 def step_cost(self, state, action): # The cost of an action taken at a given state
 
 def get_path_cost(self, actions): # The overal cost given all actions taken since the initial state
+
+
+
+# DEFINITION OF A STATE
+
+# ---- Arguments
+grid # The world grid
+_mower_pos # The position of the mower in the grid
+_earth_mode # Boolean, True if the mower can mow until the earth
+_nb_propellers # Number of propellers of the mower
+_bonus_mow # The bonus given when we mow
+_score # The score of the mower
+_penalties # Dictionary of penalties (living_penalty, mow_to_earth, lose_propeller)
+
+# ---- Methods
+def get_grid_size(self): # Height and widht of the grid
+
+def is_game_over(self): # Boolean, True if nb_propellers = 0
+
+def is_win(self): # Boolean, True if there is no mode 'H' in the grid
+
+def execute_action(self, action): # Returns final state given a set of actions
